946d54121c3a6d427dbd14f9d2cd5f0ad0cd4973
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-04-16 02:19:57 +0900

    [modify] :art:

--- app/domain/model/base.go
+++ app/domain/model/base.go
@@ -3,13 +3,13 @@ package model
 import (
 	"time"
 
-	uuid "github.com/satori/go.uuid"
+	"github.com/infobloxopen/atlas-app-toolkit/rpc/resource"
 )
 
 // Base contains common columns for all tables.
 type Base struct {
-	ID        uuid.UUID  `gorm:"type:uuid;primary_key;"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"update_at"`
-	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+	ID        *resource.Identifier `gorm:"type:uuid;primary_key;"`
+	CreatedAt time.Time            `json:"created_at"`
+	UpdatedAt time.Time            `json:"update_at"`
+	DeletedAt *time.Time           `sql:"index" json:"deleted_at"`
 }
