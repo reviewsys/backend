72488adfd86bb9ca0494ebb154739b90b45801c8
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-04-16 02:13:56 +0900

    [modify] :art:

---
+++ app/domain/model/base.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// Base contains common columns for all tables.
+type Base struct {
+	ID        uuid.UUID  `gorm:"type:uuid;primary_key;"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"update_at"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+}
--- app/domain/model/user.go
+++ app/domain/model/user.go
@@ -1,14 +1,8 @@
 package model
 
-import (
-	"time"
-)
-
 type User struct {
-	ID        int64     `json:"id"`
-	TeamID    int64     `json:"team_id"`
-	Name      string    `json:"name"`
-	IsAdmin   bool      `json:"is_admin"`
-	UpdatedAt time.Time `json:"updated_at"`
-	CreatedAt time.Time `json:"created_at"`
+	Base
+	TeamID  int64  `json:"team_id"`
+	Name    string `json:"name"`
+	IsAdmin bool   `gorm:";not null;default:false" json:"is_admin"`
 }
